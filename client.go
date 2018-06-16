3621ce3bc1c6dbe3a9a12adf2060cb095d5b6c60
Nick Pollett <nick.pollett@vendhq.com>
2018-06-16 14:50:08 +1200

    Add GetNewsForApp func

--- client.go
+++ client.go
@@ -1,5 +1,13 @@
 package steam
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+const baseURL = "http://api.steampowered.com"
+
 // Client is a basic Steam API client
 type Client struct {
 	apiKey string
@@ -10,4 +18,18 @@ func NewSteamClient(apiKey string) *Client {
 	return &Client{
 		apiKey: apiKey,
 	}
+}
+
+func (client *Client) get(url string, result interface{}) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(respBytes, &result)
 }
--- news.go
+++ news.go
@@ -1,5 +1,10 @@
 package steam
 
+import (
+	"net/url"
+	"strconv"
+)
+
 // NewsItem represents a NewsItem from the Steam API
 type NewsItem struct {
 	GID           *string    `json:"gid"`
@@ -13,4 +18,30 @@ type NewsItem struct {
 	FeedName      *string    `json:"feedname"`
 	FeedType      *string    `json:"feed_type"`
 	AppID         *string    `json:"appid"`
+}
+
+type newsItemResponse struct {
+	AppNews appNews `json:"appnews"`
+}
+
+type appNews struct {
+	NewsItems []*NewsItem `json:"newsitems"`
+}
+
+func (client *Client) GetNewsForApp(appID string, count, maxLength int64) ([]*NewsItem, error) {
+	parameters := url.Values{}
+	parameters.Add("appid", appID)
+	parameters.Add("count", strconv.FormatInt(count, 10))
+	parameters.Add("maxlength", strconv.FormatInt(maxLength, 10))
+
+	url := baseURL + "/ISteamNews/GetNewsForApp/v0002/?"
+	url += parameters.Encode()
+
+	var resp newsItemResponse
+	err := client.get(url, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.AppNews.NewsItems, nil
 }
