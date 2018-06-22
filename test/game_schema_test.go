fb0057c1653b4749a34a896f43f586cc7d8032b2
Nick Pollett <nick.pollett@vendhq.com>
2018-06-22 15:14:29 +1200

    gofmt

--- test/game_schema_test.go
+++ test/game_schema_test.go
@@ -21,7 +21,7 @@ func TestGameSchema(t *testing.T) {
 		Version: "538",
 		AvailableGameStats: &GameStats{
 			Achievements: []*Achievement{
-				&Achievement{
+				{
 					Name:         "hot_wheels",
 					DefaultValue: 0,
 					DisplayName:  "Coming in Hot",
@@ -30,7 +30,7 @@ func TestGameSchema(t *testing.T) {
 					IconURL:      "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/218620/f6ed9cd6ec9750bcd36193c74e6f16104f6c1267.jpg",
 					IconGrayURL:  "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/218620/c336adacd88a21a6010c9b5596192322aecaf265.jpg",
 				},
-				&Achievement{
+				{
 					Name:         "fish_ai",
 					DefaultValue: 0,
 					DisplayName:  "Fish A.I.",
@@ -41,12 +41,12 @@ func TestGameSchema(t *testing.T) {
 				},
 			},
 			Stats: []*Stat{
-				&Stat{
+				{
 					Name:         "player_time",
 					DefaultValue: 0,
 					DisplayName:  "",
 				},
-				&Stat{
+				{
 					Name:         "player_time_0h",
 					DefaultValue: 0,
 					DisplayName:  "",
